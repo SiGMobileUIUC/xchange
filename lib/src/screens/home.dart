5b538d3632065eb6550311286afd0e9be5790c8a
scdivad <davidsc2@illinois.edu>
2021-11-14 10:06:07 -0600

    Toggle button color added

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:xchange/src/widgets/request_item.dart';
-import 'dart:math';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -11,8 +10,7 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  int _counter = 0;
-  dynamic type;
+  int type = 0; // 0 = neither, 1 = post, 2 = request
 
   var titleController = TextEditingController();
   var descriptionController = TextEditingController();
@@ -23,19 +21,6 @@ class _HomeState extends State<Home> {
   List<int> colorCodes = <int>[];
 
   void _addPopup() {
-    _counter++;
-
-    Widget toggleType = Row(
-      children: <Widget>[
-        Expanded(
-            child: ElevatedButton(
-                onPressed: () => type = false, child: const Text("Request"))),
-        Expanded(
-            child: ElevatedButton(
-                onPressed: () => type = true, child: const Text("Post"))),
-      ],
-    );
-
     Widget submitButton = Padding(
       padding: const EdgeInsets.all(8.0),
       child: ElevatedButton(
@@ -47,70 +32,109 @@ class _HomeState extends State<Home> {
             RequestItem(
               title: title,
               description: description,
-              t: type,
+              type: type,
             ),
           );
-          colorCodes.add(min(_counter * 100, 900));
-          setState(() {});
+          // colorCodes.add(min(_counter * 100, 900));
+
+          setState(() {
+            colorCodes.add(0);
+          });
         },
       ),
     );
 
-    Widget form = Form(
-      key: _formKey,
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          toggleType,
-          // Form field 1
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextFormField(
-              controller: titleController,
-            ),
-          ),
-          // Form field 2
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextFormField(
-              controller: descriptionController,
-            ),
-          ),
-          submitButton,
-        ],
-      ),
-    );
-
-    Widget redXbutton(BuildContext context) {
-      return Positioned(
-        right: -10.0,
-        top: -50.0,
-        child: InkResponse(
-          onTap: () => Navigator.of(context).pop(),
-          child: const CircleAvatar(
-            child: Icon(Icons.close),
-            backgroundColor: Colors.red,
-          ),
-        ),
-      );
+    List<Widget> toggleRequestPost(setState) {
+      return [
+        Expanded(
+            child: ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    type = 2;
+                  });
+                },
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(
+                    type == 2 ? Colors.purple : Colors.grey,
+                  ),
+                ),
+                child: const Text("Request"))),
+        Expanded(
+            child: ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    type = 1;
+                  });
+                },
+                style: ElevatedButton.styleFrom(
+                    primary: type == 1 ? Colors.purple : Colors.grey),
+                child: const Text("Post"))),
+      ];
     }
 
     showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          shape: const RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(Radius.circular(25.0))),
-          title: const Text('billy bob and friends'),
-          content: Stack(
-            clipBehavior: Clip.none,
-            children: <Widget>[redXbutton(context), form],
-          ),
-        );
-      },
-    );
+        context: context,
+        builder: (BuildContext context) {
+          return StatefulBuilder(builder: (context, setState) {
+            return AlertDialog(
+              shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(25.0))),
+              title: const Text('billy bob and friends'),
+              content: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Row(
+                      children: toggleRequestPost(setState),
+                    ),
+                    // Form field 1
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextFormField(
+                        controller: titleController,
+                      ),
+                    ),
+                    // Form field 2
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextFormField(
+                        controller: descriptionController,
+                      ),
+                    ),
+                    submitButton,
+                  ],
+                ),
+              ),
+            );
+          });
+        });
+  }
 
-    setState(() {});
+  Widget _buildColumn(int index) {
+    return ListTile(
+        title: entries[index],
+        trailing: const Icon(
+          Icons.star,
+          color: Colors.red,
+          semanticLabel: "delete",
+        ),
+        onTap: () {
+          setState(() {
+            entries.remove(entries[index]);
+          });
+        });
+  }
+
+  Widget _buildItems() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemCount: entries.length * 2,
+        itemBuilder: /*1*/ (context, i) {
+          if (i.isOdd) return const Divider(); /*2*/
+          final index = i ~/ 2; /*3*/
+          return _buildColumn(index);
+        });
   }
 
   @override
@@ -119,17 +143,7 @@ class _HomeState extends State<Home> {
       appBar: AppBar(
         title: const Text("billy bob and friend"),
       ),
-      body: ListView.builder(
-        padding: const EdgeInsets.all(8),
-        itemCount: entries.length,
-        itemBuilder: (BuildContext context, int index) {
-          return Container(
-            height: 50,
-            color: Colors.amber[colorCodes[index]],
-            child: Center(child: entries[index]),
-          );
-        },
-      ),
+      body: _buildItems(),
       floatingActionButton: FloatingActionButton(
         onPressed: _addPopup,
         tooltip: 'Add a Request/Post',
